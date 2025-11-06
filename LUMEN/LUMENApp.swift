be722932ef6c04ab1e67eeced3ce97f7a6e4998c
Emer <esbarrientos64uth.hn>
2025-11-05 19:35:03 -0600

    Initial Commit

---
+++ LUMEN/LUMENApp.swift
@@ -0,0 +1,17 @@
+//
+//  LUMENApp.swift
+//  LUMEN
+//
+//  Created by Nimbux Solutions on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LUMENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
